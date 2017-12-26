08cd959424c119e84ae5e8635f95ffdd107cb936
Hoang Cuong <hoangcuong2011@gmail.com>
2017-12-26 13:52:20 -0500

    Update metalearning.py

--- metalearning.py
+++ metalearning.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
-DIMS = 10  # Dimensions of the parabola
-scale = tf.random_uniform([DIMS], 0.5, 1.5)
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+DIMS = 1  # Dimensions of the parabola
+scale = tf.random_uniform([DIMS], -1.5, 1.5)
 # This represents the network we are trying to optimize,
 # the `optimizee' as it's called in the paper.
 # Actually, it's more accurate to think of this as the error
@@ -20,6 +24,26 @@ def g_rms(gradients, state, learning_rate=0.1, decay_rate=0.99):
     update = -learning_rate*gradients / (tf.sqrt(state)+1e-5)
     return update, state
     
+
+LAYERS = 1
+STATE_SIZE = 20
+
+cell = tf.contrib.rnn.MultiRNNCell(
+    [tf.contrib.rnn.LSTMCell(STATE_SIZE) for _ in range(LAYERS)])
+cell = tf.contrib.rnn.InputProjectionWrapper(cell, STATE_SIZE)
+cell = tf.contrib.rnn.OutputProjectionWrapper(cell, 1)
+cell = tf.make_template('cell', cell)
+
+def g_rnn(gradients, state):
+    # Make a `batch' of single gradients to create a 
+    # "coordinate-wise" RNN as the paper describes. 
+    gradients = tf.expand_dims(gradients, axis=1)
+ 
+    if state is None:
+        state = [[tf.zeros([DIMS, STATE_SIZE])] * 2] * LAYERS
+    update, state = cell(gradients, state)
+    # Squeeze to make it a single batch again.
+    return tf.squeeze(update, axis=[1]), state
     
 TRAINING_STEPS = 20  # This is 100 in the paper
 
@@ -39,25 +63,24 @@ def learn(optimizer):
     return losses
     
     
-    
+
+
 sgd_losses = learn(g_sgd)
 rms_losses = learn(g_rms)
+rnn_losses = learn(g_rnn)
 
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 
 x = np.arange(TRAINING_STEPS)
-for _ in range(3): 
-    sgd_l, rms_l = sess.run([sgd_losses, rms_losses])
+for _ in range(1): 
+    sgd_l, rms_l, rnn_l = sess.run(
+        [sgd_losses, rms_losses, rnn_losses])
     p1, = plt.plot(x, sgd_l, label='SGD')
     p2, = plt.plot(x, rms_l, label='RMS')
-    plt.legend(handles=[p1, p2])
+    p3, = plt.plot(x, rnn_l, label='RNN')
+    plt.legend(handles=[p1, p2, p3])
     plt.title('Losses')
     plt.show()
-
-
